71c6b9f76b410bea2a5c752ca670f414a135a719
Gaurav Singh <kanwasigaurav96@gmail.com>
2026-01-16 22:40:50 +0530

    Add main function to display array elements

---
+++ arr1.rs
@@ -0,0 +1,11 @@
+fn main() {
+
+    let arr1: [i32; 5] = [10, 20, 30, 40, 50];
+
+
+    println!("Array elements: {:?}", arr1);
+
+
+    println!("First element: {}", arr1[0]);
+    println!("Last element: {}", arr1[4]);
+}
