7a65f73147ccf6c04ea3fa127335e2c4c2f36d03
Gaurav Singh <kanwasigaurav96@gmail.com>
2026-09-21 21:43:09 +0530

    Modify loopj function to return double the count

--- loopj.rs
+++ loopj.rs
@@ -1,17 +1,23 @@
-fn loopj() {
+fn loopj() -> u32 {
     let mut count = 0;
 
     println!("Starting the loop!");
 
     let result = loop {
         count += 1;
-        println!("Count is: {}", count);
+        println!("Count is: {count}");
 
         if count == 5 {
             println!("Reached 5, exiting loop.");
-            break count;
+            break count * 2;
         }
     };
 
-    println!("Final count: {}", result);
+    println!("Final count: {result}");
+    result
+}
+
+fn main() {
+    let final_result = loopj();
+    println!("Returned from loopj: {final_result}");
 }
